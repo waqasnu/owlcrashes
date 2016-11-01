0ed33842d838d3088356b20059b84fff6f7379ed
waqasnu <waqasnu>
2016-11-01 13:16:38 +0500

    c

---
+++ Assets/_Yurowm/Match-Tree Engine/Scripts/playadvs.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Advertisements;
+using System.Collections;
+
+public class playadvs : MonoBehaviour {
+
+	// Use this for initialization
+	public void Start () {
+
+		Advertisement.Show();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
